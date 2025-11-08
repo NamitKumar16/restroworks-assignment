78f01f5aafc531b0d3e64dd93f7f6af617e2a716
Namit Kumar <kumarnamit58@gmail.com>
2025-11-08 19:21:56 +0530

    added serverUrl in payload

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -21,6 +21,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: process.env.PAYLOAD_URL || 'http://localhost:3000',
   admin: {
     user: Users.slug,
     importMap: {
